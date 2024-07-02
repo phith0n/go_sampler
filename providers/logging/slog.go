850b6ac8c4abdae0c17ebd2a48d157f09dd6f78b
phith0n <phith0n.ph2f@gmail.com>
2024-07-03 04:24:45 +0800

    fixed logical issue

--- providers/logging/slog.go
+++ providers/logging/slog.go
@@ -9,12 +9,16 @@ import (
 
 func NewLogging(cfg *config.Config) *slog.Logger {
 	var log *slog.Logger
-	options := slog.HandlerOptions{AddSource: true, Level: slog.LevelInfo}
+	options := slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+	}
+
 	if cfg.Debug {
 		options.Level = slog.LevelDebug
-		log = slog.New(slog.NewJSONHandler(os.Stderr, &options))
-	} else {
 		log = slog.New(slog.NewTextHandler(os.Stderr, &options))
+	} else {
+		log = slog.New(slog.NewJSONHandler(os.Stderr, &options))
 	}
 
 	slog.SetDefault(log)
--- web/handler.go
+++ web/handler.go
@@ -10,11 +10,16 @@ import (
 )
 
 func NewHandler(logger *slog.Logger, cfg *config.Config) *gin.Engine {
+	if cfg.Debug {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
 	r := gin.New()
 	r.Use(sloggin.New(logger), gin.Recovery())
 	r.GET("/ping", func(c *gin.Context) {
 		c.Data(200, "text/plain", []byte("pong"))
 	})
-
 	return r
 }
